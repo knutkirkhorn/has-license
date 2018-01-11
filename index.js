03d274dd6318e41134780bacf82ff8e76cc81494
Knut Kirkhorn <knutkirk@hotmail.com>
2018-01-11 15:51:16 +0100

    Check if path contains `https` or `http`

--- index.js
+++ index.js
@@ -4,7 +4,12 @@ const ghRepoHasLicense = require('gh-repo-has-license');
 
 module.exports = path => {
     return new Promise((resolve, reject) => {
-        if (path.includes('github.com')) {
+        if (path.startsWith('https://github.com/') || path.startsWith('http://github.com/')) {
+            if (path.startsWith('http://github.com/')) {
+                // Force https connection
+                path = path.substr(0, 4) + 's' + path.substr(4);
+            }
+
             // Check if the license file exists at the GitHub repository
             ghRepoHasLicense(path).then(result => {
                 resolve(result);
